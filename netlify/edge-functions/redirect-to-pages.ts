66f37dcfcee347960fca8a6e68e0dab84cb7625c
Hyeseong Kim <hey@hyeseong.kim>
2022-11-21 00:50:06 +0900

    이건???

--- netlify/edge-functions/redirect-to-pages.ts
+++ netlify/edge-functions/redirect-to-pages.ts
@@ -4,10 +4,8 @@ export default async (request: Request, context: Context) => {
   const url = new URL(request.url);
   url.hostname = 'the-future-of-frontend-is-edge.pages.dev';
 
-  return new Response('Redirected', {
-    status: 301,
-    headers: {
-      'Location': url.toString(),
-    },
-  });
+  const response = context.next();
+  response.headers.set('Location', url.toString());
+
+  return response;
 };
